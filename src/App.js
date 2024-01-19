6423b40a23e4fc23a8fa9236e71f86cafffbf1fc
Bhautik Savaliya <bhautiksavaliya2000@gmail.com>
2024-01-18 20:28:03 -0500

    Added basic UI interface

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Vacuum Window</h2>
       </header>
+      <div class="main-container">
+        <div class="sub-container">
+            <div class='address_container'>
+              <p class="label">Your address</p>
+              <p class="address">234 545 876</p>
+            </div>
+            
+            <div class='create_conn_container'>
+              <p class="label">Enter connection address</p>
+              <input type='number' class="Entry" />
+            </div>
+            
+        </div>
+        <div class="sub-container">
+          <textarea>
+
+          </textarea>
+        </div>
+      </div>
     </div>
   );
 }
